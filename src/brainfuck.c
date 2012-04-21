6134f84c30a080b99050f4fc4dc289bdbb9f9253
Fabian M <mail.fabianm@gmail.com>
2012-04-21 18:03:39 +0200

    Removed unnecessary inclusions.

--- src/brainfuck.c
+++ src/brainfuck.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #ifndef BRAINFUCK_H
 #include "brainfuck.h"
 #endif
+
 #define MAX_CELLS 65536
 
 void brainfuck_eval(char chars[]) {
