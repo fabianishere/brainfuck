f100748a0fe1e1c7fd91bc5d0a8370164bb6cc46
Fabian M <mail.fabianm@gmail.com>
2013-03-29 16:28:42 +0100

    Updated man page..

--- src/brainfuck_shell.c
+++ src/brainfuck_shell.c
@@ -22,8 +22,6 @@ void brainfuck_file(char filename[]) {
 	size = ftell(file);
 	fseek(file, 0, SEEK_SET);
 	char chars[size];
-	/* Strange fix required in order to run most brainfuck programs... */
-	chars[pointer++] = ' ';
 	/* Place each character from the file into the array */
 	while ((c = fgetc(file)) != EOF)
 		chars[pointer++] = (char) c;
@@ -34,23 +32,8 @@ void brainfuck_file(char filename[]) {
 
 int main(int argc, char *argv[]) {
 	if (argc < 2) {
-		printf("Usage: brainfuck [-fi] <filename>\n");
+		printf("Usage: brainfuck [-fc] <filename>\n\t-f  <filename>\n\t-c  Run code directly\n");
 		return EXIT_FAILURE;
-	} else if(strcmp(argv[1], "-i") == 0) {
-		printf("Entering interactive mode...\n");
-		printf("Welcome to the Brainfuck Interpreter v0.2!\n");
-		char c;
-		int pointer = 0;
-		for(;;) {
-			printf("brainfuck> ");
-			int size = 36000;
-			char chars[size];
-			chars[pointer++] = ' ';
-			while ((c = getchar()) != 10)
-				chars[pointer++] = (char) c;
-			/* Run the code */
-			brainfuck_eval(chars);
-		}
 	} else if(strcmp(argv[1], "-f") == 0) {
 		if(argc < 3)
 		{
@@ -60,8 +43,17 @@ int main(int argc, char *argv[]) {
 		{
 			brainfuck_file(argv[2]);
 		}
+	} else if(strcmp(argv[1], "-c") == 0) {
+		if(argc < 3)
+		{
+			printf("Error: No code specified!\n");
+		}
+		else
+		{
+			brainfuck_eval(argv[2]);
+		}
 	} else {
-		printf("Error: Invalid command line option!\nUsage: brainfuck [-fi] <filename>\n\t-f  <filename>\n\t-i  Interactive Mode\n");
+		printf("Error: Invalid command line option!\nUsage: brainfuck [-fc] <filename>\n\t-f  <filename>\n\t-c  Run code directly\n");
 	}
 	return EXIT_SUCCESS;
 }
