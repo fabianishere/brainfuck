fabianishere/brainfuck

commit 5b33047702506d9aea637511bf41c38420692d99

SECURITY FIX: memory corruption tape over/underrun
  Checking that a brainfuck program only accesses
  it's tape memory. Fixes arbitrary heap memory
  corruption exploit.

src/brainfuck.c
@@ -36,14 +36,22 @@ BrainfuckState * brainfuck_state() {
  * @param size The size of the tape.
  */
 BrainfuckExecutionContext * brainfuck_context(int size) {
-	BrainfuckExecutionContext *context = (BrainfuckExecutionContext *) 
-			malloc(sizeof(BrainfuckExecutionContext));
 	if (size < 0) {
 		size = BRAINFUCK_TAPE_SIZE;
 	}
-	context->tape = malloc(sizeof(BRAINFUCK_CELL_TYPE) * size);
-	context->input_handler = &getchar;
-	context->output_handler = &putchar;
+	char* tape = malloc(sizeof(BRAINFUCK_CELL_TYPE) * size);
+    //create prototype to initialize the const fields
+    BrainfuckExecutionContext context_prototype = {
+        .output_handler = &putchar,
+        .input_handler = &getchar,
+        .tape = tape,
+        .tape_index = 0,
+        .tape_size = size
+        };
+    // copy to allocated memory
+	BrainfuckExecutionContext *context = (BrainfuckExecutionContext *) 
+			malloc(sizeof(BrainfuckExecutionContext));
+	memcpy(context, &context_prototype, sizeof(BrainfuckExecutionContext));
 	return context;
 }
 
@@ -487,10 +495,18 @@ void brainfuck_execute(BrainfuckInstruction *root, BrainfuckExecutionContext *co
 			*context->tape -= instruction->quantity;
 			break;
 		case BRAINFUCK_TOKEN_NEXT:
-			context->tape += instruction->quantity;
+			context->tape_index += instruction->quantity;
+			if (context->tape_index >= context->tape_size) {
+			    fprintf(stderr, "Tape memory out of bounds (overrun)\nExceeded the tape size of %zd cells\n", context->tape_size);
+			    exit(-1);
+			}
 			break;
 		case BRAINFUCK_TOKEN_PREVIOUS:
-			context->tape -= instruction->quantity;
+			context->tape_index -= instruction->quantity;
+			if (context->tape_index < 0) {
+			    fprintf(stderr, "Tape memory out of bounds (underrun)\nUndershot the tape size of %zd cells\n", context->tape_size);
+			    exit(-1);
+			}
 			break;
 		case BRAINFUCK_TOKEN_OUTPUT:
 			for (index = 0; index < instruction->quantity; index++) {

include/brainfuck.h
@@ -101,7 +101,15 @@ typedef struct BrainfuckExecutionContext {
 	/*
 	 * An array containing the memory cells the program can use.
 	 */
-	char *tape;
+	char * const tape;
+	/*
+	 * Index into <code>tape</code>. Modified during execution.
+	 */
+	int tape_index;
+	/*
+	 * size of the tape in umber of cells.
+	 */
+	const size_t tape_size;
 } BrainfuckExecutionContext;
 
 /*

commit cf78c961255b6e9f1484b131cfcf2cc99bae9f60

Fixed: accessing correct tape index
  Bug introduced in 5b33047702

src/brainfuck.c
@@ -520,10 +520,10 @@ void brainfuck_execute(BrainfuckInstruction *root, BrainfuckExecutionContext *co
 	while (instruction != NULL && instruction->type != BRAINFUCK_TOKEN_LOOP_END) {
 		switch (instruction->type) {
 		case BRAINFUCK_TOKEN_PLUS:
-			*context->tape += (unsigned char)instruction->quantity; // may overflow
+			context->tape[context->tape_index] += (unsigned char)instruction->quantity; // may overflow
 			break;
 		case BRAINFUCK_TOKEN_MINUS:
-			*context->tape -= (unsigned char)instruction->quantity; // may underflow
+			context->tape[context->tape_index] -= (unsigned char)instruction->quantity; // may underflow
 			break;
 		case BRAINFUCK_TOKEN_NEXT:
 			if (instruction->quantity >= INT_MAX - context->tape_size || 
@@ -543,15 +543,15 @@ void brainfuck_execute(BrainfuckInstruction *root, BrainfuckExecutionContext *co
 			break;
 		case BRAINFUCK_TOKEN_OUTPUT:
 			for (index = 0; index < instruction->quantity; index++) {
-				context->output_handler(*context->tape);
+				context->output_handler(context->tape[context->tape_index]);
 			}
 			break;
 		case BRAINFUCK_TOKEN_INPUT:
 			for (index = 0; index < instruction->quantity; index++)
-				*context->tape = context->input_handler();
+				context->tape[context->tape_index] = context->input_handler();
 			break;
 		case BRAINFUCK_TOKEN_LOOP_START:
-			while(*context->tape)
+			while(context->tape[context->tape_index])
 				brainfuck_execute(instruction->loop, context);
 			break;
 		default: