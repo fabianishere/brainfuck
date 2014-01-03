9c5834bc9f4dfa3ec5db65544786f926a5349124
Fabian M. <mail.fabianm@gmail.com>
2014-01-03 21:52:29 +0100

    Update copyright year

--- src/brainfuck.c
+++ src/brainfuck.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 Fabian M.
+ * Copyright 2014 Fabian M.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
