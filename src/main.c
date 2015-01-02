9dd53bc7e3ffe2eff91b504ab89af06cff7de645
Fabian M <mail.fabianm@gmail.com>
2015-01-02 21:56:59 +0100

    Update copyright notice

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Fabian M.
+ * Copyright 2015 Fabian M.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
