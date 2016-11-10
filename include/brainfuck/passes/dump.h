6b818ef4f015f046792f920ff28d44989c4cb3e7
Fabian Mastenbroek <mail.fabianm@gmail.com>
2016-11-10 17:16:20 +0100

    create interface for dump analysis pass

---
+++ include/brainfuck/passes/dump.h
@@ -0,0 +1,50 @@
+/*
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2016 Fabian Mastenbroek
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+/*
+ * A pass that dumps the intermediate representation of the script to the given
+ * file stream.
+ * The macro BRAINFUCK_PASS_DUMP_AVAILABLE should be defined by implementations
+ * supporting this pass.
+ */
+#ifndef BRAINFUCK_PASS_DUMP_H
+#define BRAINFUCK_PASS_DUMP_H
+
+/** @addtogroup pass Pass */
+/** @{ */
+
+/**
+ * A flag that indicates whether this pass is available.
+ */
+const int brainfuck_pass_dump_available;
+
+/**
+ * This pass dumps the intermediate representation of the given scripts to the
+ * given file stream in a x86 GNU AS assembly format.
+ *
+ * @param[in] script The script to dump.
+ * @param[in] file The file to dump the intermediate representation to.
+ */
+void brainfuck_pass_dump(const struct BrainfuckScript *script, FILE *file);
+/** @} */
+#endif /* BRAINFUCK_PASS_DUMP_H */
